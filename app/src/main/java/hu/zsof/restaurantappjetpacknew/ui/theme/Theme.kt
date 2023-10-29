269866262fa526b562f86c11325d216762c8736e
zsof <zsofi.viki@gmail.com>
2023-10-29 14:18:07 +0100

    Outsource colors

--- app/src/main/java/hu/zsof/restaurantappjetpacknew/ui/theme/Theme.kt
+++ app/src/main/java/hu/zsof/restaurantappjetpacknew/ui/theme/Theme.kt
@@ -4,43 +4,42 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Color(0xff03a9f4), // gombok alapháttere
-    onPrimary = Color(0xFF001240),
-    primaryContainer = Color(0xff03a9f4),
-    onPrimaryContainer = Color(0xFFFFFFFF),
-    secondary = primaryDark, // tab bar-nak a színe
-    onSecondary = Color(0xFFDDDADA),
-    secondaryContainer = Color(0xff03a9f4), // bottonbaron az ikonok háttere,
-    onSecondaryContainer = Color(0xFFFFFFFF), // bottonbaron az ikon körvonala
-    tertiary = Color(0xFFCA0C0C), // ??
-    onTertiary = Color(0xFFCA0C0C), // ??
-    tertiaryContainer = Color(0xFFCA0C0C), // ??
-    onTertiaryContainer = Color(0xFFCA0C0C), // ??
-    error = Color(0xFFCA0C0C),
+    primary = PrimaryDark, // gombok alapháttere
+    onPrimary = OnPrimaryDark,
+    primaryContainer = PrimaryContainerDark,
+    onPrimaryContainer = OnPrimaryContainerDark,
+    secondary = SecondaryDark, // tab bar-nak a színe
+    onSecondary = OnSecondaryDark,
+    secondaryContainer = SecondaryContainerDark, // bottonbaron az ikonok háttere,
+    onSecondaryContainer = OnSecondaryContainerDark, // bottonbaron az ikon körvonala
+    tertiary = TertiaryDark, // ??
+    onTertiary = OnTertiaryDark, // ??
+    tertiaryContainer = TertiaryContainerDark, // ??
+    onTertiaryContainer = OnTertiaryContainerDark, // ??
+    error = ErrorDark,
     onError = OnErrorDark,
     errorContainer = ErrorContainerDark,
     onErrorContainer = OnErrorContainerDark,
-    background = Color(0xFF000000), // ez csak akkor ha Scaffold van
-    // onBackground = Color(0xFF000000),
-    surface = Color(0xFF2D2D2E), // ha surface-ben a content-ben van --> a háttér
-    outline = Color(0xFFFFFFFF), // button, textfieldek körvonala
-    surfaceVariant = Color(0xFF2D2D2E), // card színe!!,  slideren a vonal, görgetős háttere
-    onSurfaceVariant = Color(0xFFFFFFFF), // iconok színe, hint-ek színe, checkob színe, egyéb szövegek színe
+    background = BackgroundDark, // ez csak akkor ha Scaffold van
+    // onBackground = OnBackgroundDark,
+    surface = SurfaceDark, // ha surface-ben a content-ben van --> a háttér
+    outline = OutlineDark, // button, textfieldek körvonala
+    surfaceVariant = SurfaceVariantDark, // card színe!!,  slideren a vonal, görgetős háttere
+    onSurfaceVariant = OnSurfaceVariantDark, // iconok színe, hint-ek színe, checkob színe, egyéb szövegek színe
 )
 
 private val LightColorScheme = lightColorScheme(
 
-    primary = Color(0xFFFFC107),
+    primary = PrimaryLight,
     onPrimary = OnPrimaryLight,
-    primaryContainer = Color(0xFFFFC107),
+    primaryContainer = PrimaryContainerLight,
     onPrimaryContainer = OnPrimaryContainerLight,
-    secondary = Color(0xFFFFC107),
-    onSecondary = Color(0xFF666565),
-    secondaryContainer = Color(0xFFFFFFFF),
+    secondary = SecondaryLight,
+    onSecondary = OnSecondaryLight,
+    secondaryContainer = SecondaryContainerLight,
     onSecondaryContainer = OnSecondaryContainerLight,
     tertiary = TertiaryLight,
     onTertiary = OnTertiaryLight,
@@ -50,12 +49,12 @@ private val LightColorScheme = lightColorScheme(
     onError = OnErrorLight,
     errorContainer = ErrorContainerLight,
     onErrorContainer = OnErrorContainerLight,
-    background = Color(0xFFE7E6E6),
-    onBackground = Color(0xD8705D5D),
-    surface = BackgroundLight,
+    background = BackgroundLight,
+    onBackground = OnBackgroundLight,
+    surface = SurfaceLight,
     outline = OutlineLight,
-    surfaceVariant = Color(0xFFFFFFFF),
-    onSurfaceVariant = Color(0xFF000000),
+    surfaceVariant = SurfaceVariantLight,
+    onSurfaceVariant = OnSurfaceVariantLight,
 )
 
 @Composable
