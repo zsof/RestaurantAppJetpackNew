81612681ed39a8d1a24a731cb44ee8b4d7638895
zsof <zsofi.viki@gmail.com>
2023-11-21 21:24:00 +0100

    Fix Authorization header empty at first start

--- app/src/main/java/hu/zsof/restaurantappjetpacknew/MainViewModel.kt
+++ app/src/main/java/hu/zsof/restaurantappjetpacknew/MainViewModel.kt
@@ -1,16 +1,20 @@
 package hu.zsof.restaurantappjetpacknew
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import hu.zsof.restaurantappjetpacknew.module.AppState
 import hu.zsof.restaurantappjetpacknew.network.repository.AuthRepository
+import hu.zsof.restaurantappjetpacknew.util.Constants
 import hu.zsof.restaurantappjetpacknew.util.SharedPreferences
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
+    @ApplicationContext private val context: Context,
     private val sharedPref: SharedPreferences,
     private val authRepository: AuthRepository,
 ) : ViewModel() {
@@ -25,9 +29,17 @@ class MainViewModel @Inject constructor(
     val user = AppState.loggedUser
 
     fun authenticateLoggedUser() {
-        viewModelScope.launch {
-            val loggedUser = authRepository.authUser()
-            user.postValue(loggedUser.user)
+        val sharedPreferences =
+            context.getSharedPreferences(
+                Constants.Prefs.AUTH_SHARED_PREFERENCES,
+                Context.MODE_PRIVATE,
+            )
+        val token = sharedPreferences.getString("bearer", "")
+        if (token.isNullOrEmpty().not()) {
+            viewModelScope.launch {
+                val loggedUser = authRepository.authUser()
+                user.postValue(loggedUser.user)
+            }
         }
     }
 }
